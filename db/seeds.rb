6744b0436fcd68a8e74858d329749dfc32b29ca8
Jen Fox <jenniferfox4@gmail.com>
2018-10-08 14:16:04 -0400

    Created `seeds.rb` - Takes in CSV mountains.csv - Creates new Mountain with all variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'mountains.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Mountain.new
+  t.name = row['MOUNTAIN']
+  t.elevation = row['ELEVATION']
+  t.state = row['STATE']
+  t.difficulty = row['DIFFICULTY']
+  t.distance_to_summit = row['DISTANCE']
+  t.latitude = row['LATITUDE']
+  t.longitude = row['LONGITUDE']
+  t.features = row['FEATURES']
+
+  t.save
+  puts "#{t.name} saved"
+end
+puts csv_text
