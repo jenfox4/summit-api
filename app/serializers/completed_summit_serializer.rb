2497f3a12c613a960bba406d313f40a8454a0ae6
Jen Fox <jenniferfox4@gmail.com>
2018-10-10 16:05:08 -0400

    Altered serializer so that json returns completed_summits for each mountain

--- app/serializers/completed_summit_serializer.rb
+++ app/serializers/completed_summit_serializer.rb
@@ -1,5 +1,5 @@
 class CompletedSummitSerializer < ActiveModel::Serializer
-  attributes :id, :notes
-  has_one :user
-  has_one :mountain
+  attributes :id, :notes, :user
+  belongs_to :user
+  belongs_to :mountain
 end
--- app/serializers/mountain_serializer.rb
+++ app/serializers/mountain_serializer.rb
@@ -1,3 +1,4 @@
 class MountainSerializer < ActiveModel::Serializer
   attributes :id, :name, :elevation, :state, :difficulty, :distance_to_summit, :latitude, :longitude, :features
+  has_many :completed_summits
 end
